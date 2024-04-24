a9609fa415272e48e3d5fe882b30fb21344c1519
fredrik carlsson <fresidue@gmail.com>
2024-04-24 16:10:40 +0200

    improved typing

--- openscad-reporter.d.ts
+++ openscad-reporter.d.ts
@@ -1,19 +1,38 @@
 
 
 declare module 'openscad-reporter' {
-  export function runScad({
-    inputFile,
-    jsonOutputFile,
-    outputFile,
-    scadInjection,
-    recordTag,
-    isQuiet,
-  }: {
+
+  export type ScadConfig = {
     inputFile: string,
     jsonOutputFile?: string,
     outputFile?: string,
     scadInjection?: string,
     recordTag?: string,
     isQuiet?: boolean,
-  }): void;
+  };
+
+  export type ScadSummary = {
+    geometriesNum: number,
+    geometriesCacheSizeBytes: number,
+    cgalPolyhedronsNum: number,
+    cgalCacheSizeBytes: number,
+    isSimple: boolean,
+    numVertices: number,
+    numHalfedges: number,
+    numEdges: number,
+    numHalffacets: number,
+    numFacets: number,
+    numVolumes: number,
+  };
+  
+  export type ScadRes = {
+    data: object,
+    summary: ScadSummary,
+    echoLines: string[],
+    recorderLines: string[],
+    openscadLines: string[],
+    warningLines: string[],
+  };
+  
+  export async function runScad(config: ScadConfig): ScadRes;
 }
